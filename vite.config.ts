38c23e1e6683da13eef407ca0ad40129b4f4f3ad
fistar0823-crypto <fistar0823@gmail.com>
2025-09-15 15:53:55 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ww/',   // 這裡很重要，要跟 GitHub repo 名稱相同
+  base: '/ww/'
 })
