4bb6de1540ea86823afe1376f75e4928e554d787
fistar0823-crypto <fistar0823@gmail.com>
2025-09-16 08:25:40 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ww/'
+  base: '/ww/',  // ⚠️ Repo 名稱是 ww，所以一定要加這個
+  build: {
+    outDir: 'dist',  // 打包輸出到 dist
+    emptyOutDir: true
+  }
 })
