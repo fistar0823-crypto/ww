7d077642e94f84896d7009285f0320d4595d55af
fistar0823-crypto <fistar0823@gmail.com>
2025-09-17 10:11:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ⚠️ base 必須跟 repo 名稱相同 → '/ww/'
 export default defineConfig({
   plugins: [react()],
   base: '/ww/',
-  build: { outDir: 'dist', sourcemap: true }
 })
