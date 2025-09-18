266bc063fbf588f3b75777d65fd505bd5dc4f982
fistar0823-crypto <fistar0823@gmail.com>
2025-09-18 10:50:18 +0800

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // 這裡要設為你的 GitHub repo 名稱前後加斜線
-  base: '/ww/'
-});
