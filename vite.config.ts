1a19de6a995233bc87544136050dc22303e0edfe
fistar0823-crypto <fistar0823@gmail.com>
2025-09-18 11:50:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // 這裡要設為你的 GitHub repo 名稱前後加斜線
-  base: '/ww/'
+  base: '/ww/', // 你的 repo 名稱
 });
